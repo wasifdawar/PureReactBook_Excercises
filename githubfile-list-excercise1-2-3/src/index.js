180e5b9734c0608335bab40335c2a239c46f000e
wasifdawar <m.wasifdawar@gmail.com>
2021-09-14 22:29:04 +0500

    Github file Excercise1 is done

---
+++ githubfile-list-excercise1-2-3/src/index.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Time from './time'
+import PropTypes from 'prop-types';
+import './index.css';
+
+
+const FileListItem = ({file}) => (
+  <tr className="filelist-item">
+    <td><FileIcon file={file}/></td>
+    <td><FileName file={file}/></td>
+    <td><CommitMessage commit={file.latestCommit}/></td>
+    <td><Time time={file.updated_at}/></td>
+  </tr>
+);
+FileListItem.propTypes = {
+  file: PropTypes.object.isRequired
+};
+
+const FileList =({ files })=>(
+  <table className="filelist">
+    <tbody>
+      {files.map(file =>
+        <FileListItem key ={file.id} file={file}/>
+                 
+      )}
+    </tbody>
+  </table>
+);
+
+FileList.propTypes = {
+  files: PropTypes.array
+};
+
+
+const CommitMessage = ({ commit }) => (
+  <span className="commitmessage">
+    {commit.message}
+  </span>
+);
+
+CommitMessage.propTypes = {
+  commit: PropTypes.object.isRequired
+};
+
+function FileIcon({file})
+{
+  let icon = 'fa-file-text-o';
+  if (file.type === 'folder') {
+    icon='fa-folder'
+  }
+  return (
+    <span className="file-icon">
+      <i className= {`fa ${icon}`}/>
+    </span>
+  )
+}
+
+FileIcon.propTypes = {
+  file: PropTypes.object.isRequired
+};
+
+const FileName=({ file })=> (
+
+    <FileIcon file={file}  />,
+    <span className="file-name" >{file.name}</span>
+    )
+
+
+
+const testFiles = [
+  {
+    id: 1,
+    name: 'src',
+    type: 'folder',
+    updated_at: "2021-07-11 21:24:00",
+    latestCommit: {
+    message: 'Initial commit'
+    }
+    },
+    {
+    id: 2,
+    name: 'tests',
+    type: 'folder',
+    updated_at: "2021-08-11 21:24:00",
+    latestCommit: {
+    message: 'Initial commit'
+    }
+    },
+    {
+    id: 3,
+    name: 'README',
+    type: 'file',
+    updated_at: "2021-09-12 21:24:00",
+    latestCommit: {
+    message: 'Added a readme'
+    }
+    },
+]
+
+
+
+
+ReactDOM.render(
+  <FileList files={testFiles}/>,
+  document.getElementById('root')
+);
+
